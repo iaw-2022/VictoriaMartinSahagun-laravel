a47b448a8f6500764a31d43f320ee1508b69205e
VictoriaMartinSahagun <vams.00@hotmail.com>
2022-04-26 23:00:00 -0300

    Squashed commit of the following:
    commit 3cb1d4f59e321685786d92f8a58226e7ab7bd7fd
    Author: VictoriaMartinSahagun <vams.00@hotmail.com>
    Date:   Thu Apr 21 15:55:02 2022 -0300
    
        user seeder
    
    commit ae52a1430efbb954fe24db620edc5566293446aa
    Author: VictoriaMartinSahagun <vams.00@hotmail.com>
    Date:   Wed Apr 20 16:07:07 2022 -0300
    
        'configMail'
    
    commit 7bc0760ec22e2686436292bdebf1fda27ceda2a1
    Author: VictoriaMartinSahagun <vams.00@hotmail.com>
    Date:   Tue Apr 12 19:45:24 2022 -0300
    
        laravelBreeze

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name'=>'admin',
+            'email'=>'admin@admin.com',
+            'email_verified_at' => now(),
+            'password'=>bcrypt('admin'),
+            'rol'=>'admin'
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', 'App\Http\Controllers\welcomeController@index');
 
 Route::get('/dashboard', function () {
